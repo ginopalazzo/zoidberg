8b821094288fcd907a7fd9913c4c5c783c9b63d4
ginopalazzo <ginopalazzo@gmail.com>
2018-03-14 15:12:03 +0100

    preparing release 0.1.1.

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ginopalazzo/zoidberg',
-    version='0.1.2',
+    version='0.1.1',
     zip_safe=False,
 )
