23b808a4a9b7dc3bf05504fe3ef29df0eb0957f9
ginopalazzo <ginopalazzo@gmail.com>
2018-03-15 16:53:55 +0100

    Quick pypi fix

---
+++ zoidberg/scraper/__init__.py
@@ -0,0 +1,1 @@
+from .spiders.es_spider import *
--- zoidberg/zoidberg.py
+++ zoidberg/zoidberg.py
@@ -14,8 +14,8 @@ from twisted.internet import reactor, defer
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
-# from zoidberg.scraper.spiders import es_spider
-from scraper.spiders import es_spider
+from zoidberg.scraper.spiders import es_spider
+# from scraper.spiders import es_spider
 import json
 
 configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s', 'LOG_LEVEL': 'WARNING'})
