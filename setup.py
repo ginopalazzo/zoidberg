c3b4b44c85e5c2218459af759d7763adb502edaf
ginopalazzo <ginopalazzo@gmail.com>
2018-03-26 13:27:49 +0200

    doc, readme update with new CLI

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/ginopalazzo/zoidberg',
-    version='0.1.3.14',
+    version='0.1.3.15',
     zip_safe=False,
 )
